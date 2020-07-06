50bc0a61ee9d78a4e07954f54aa1242abaea195d
Monica Bui <monicahb@fb.com>
2020-07-06 10:48:40 -0700

    Login controller

---
+++ ParseChat/View Controllers/LoginViewController.m
@@ -0,0 +1,121 @@
+//
+//  LoginViewController.m
+//  ParseChat
+//
+//  Created by Monica Bui on 7/6/20.
+//  Copyright © 2020 fbu. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapLogin:(id)sender {
+    if ([self.usernameTextField.text isEqual:@""] || [self.passwordTextField.text isEqual:@""]) {
+        [self alertMissingField];
+    }
+    else {
+        NSString *username = self.usernameTextField.text;
+        NSString *password = self.passwordTextField.text;
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+                [self alertError:error.localizedDescription];
+            } else {
+                NSLog(@"User logged in successfully with username: %@", user.username);
+                
+                // display view controller that needs to shown after successful login
+            }
+        }];
+    }
+}
+
+- (IBAction)didTapSignUp:(id)sender {
+    if ([self.usernameTextField.text isEqual:@""] || [self.passwordTextField.text isEqual:@""]) {
+        [self alertMissingField];
+    }
+    else {
+        // initialize a user object
+        PFUser *newUser = [PFUser user];
+        
+        // set user properties
+        newUser.username = self.usernameTextField.text;
+        newUser.password = self.passwordTextField.text;
+        
+        // call sign up function on the object
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                [self alertError:error.localizedDescription];
+            } else {
+                NSLog(@"User registered successfully with username: %@", newUser.username);
+                
+                // manually segue to logged in view
+            }
+        }];
+    }
+}
+
+- (void)alertMissingField {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error"
+           message:@"Missing username or password"
+    preferredStyle:(UIAlertControllerStyleAlert)];
+
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+}
+
+- (void)alertError:(NSString *)errorMessage {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Please try again"
+           message:errorMessage
+    preferredStyle:(UIAlertControllerStyleAlert)];
+
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Ok"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
